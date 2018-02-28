4d9394ceea2bedf569d6c480b007bc5e9420b2f6
Gerasimos <makis.gotsop@gmail.com>
2018-02-28 09:39:29 +0200

    Project MyApplication2 for library testing has been uploaded

---
+++ MyApplication2/mylibrary/src/main/java/com/example/mylibrary/MyMath.java
@@ -0,0 +1,23 @@
+package com.example.mylibrary;
+
+/**
+ * Created by Gerasimos on 2/26/2018.
+ */
+
+public class MyMath {
+    public static int Plus(int a , int b){
+        return a*b;
+    }
+    public static int Minus(int a , int b){
+        return a-b;
+    }
+    public static int Multiplier(int a , int b){
+        return a*b;
+    }
+    public static int Divi(int a , int b){
+        return a/b;
+    }
+
+}
+
+
